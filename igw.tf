b8d720915e6cc241e36cded8206cb01710e31f13
bobbyryu <bobbyryu@gmail.com>
2021-12-29 23:14:12 -0500

    igw

---
+++ igw.tf
@@ -0,0 +1,9 @@
+# Create and Attach internet gateway
+
+resource "aws_internet_gateway" "my-igw" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name        = "my-igw"
+    Terraform   = "true"
+  }
+}
