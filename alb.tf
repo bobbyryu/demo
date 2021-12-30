c09343cbf096e0ff474fd9b711c16c35e336c5c0
bobbyryu <bobbyryu@gmail.com>
2021-12-30 00:37:27 -0500

    application load balancers

---
+++ alb.tf
@@ -0,0 +1,87 @@
+# Create Application Load Balancer Security Group #1
+
+resource "aws_security_group" "my_alb_sg_1" {
+  vpc_id = aws_vpc.my_vpc.id
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+  tags = {
+    Name        = "ALB Security Group 1"
+    Terraform   = "True"   
+  } 
+}
+
+# Create Application Load Balancer (Public Subnets)
+
+resource "aws_lb" "my_alb_1" {
+  name               = "my-app-load-balancer-1"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.my_alb_sg_1.id]
+  subnets = [
+    aws_subnet.my-public-1.id,
+    aws_subnet.my-public-2.id,
+  ]
+  enable_deletion_protection = false
+  tags = {
+    Name        = "my alb-1"
+    Terraform   = "True"   
+  } 
+}
+
+# Create Application Load Balancer Security Group #2
+
+resource "aws_security_group" "my_alb_sg_2" {
+  vpc_id = aws_vpc.my_vpc.id
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+  tags = {
+    Name        = "ALB Security Group 2"
+    Terraform   = "True"   
+  } 
+}
+
+# Create Application Load Balancer (Private Subnets)
+
+resource "aws_lb" "my_alb_2" {
+  name               = "my-app-load-balancer-2"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.my_alb_sg_2.id]
+  subnets = [
+    aws_subnet.my-private-1.id,
+    aws_subnet.my-private-2.id,
+  ]
+  enable_deletion_protection = false
+  tags = {
+    Name        = "my alb-2"
+    Terraform   = "True"   
+  } 
+}
