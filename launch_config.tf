07f7a9d3c8f22fb549eb0251772d7c6ecb36b944
betulbera <receppek32@gmail.com>
2022-01-02 19:16:52 -0500

    user data added

--- launch_config.tf
+++ launch_config.tf
@@ -3,6 +3,7 @@ resource "aws_launch_configuration" "my_launch_config" {
   image_id      = var.image_id
   instance_type = var.instance_type
   security_groups = [aws_security_group.my_asg_sg_1.id]
+  user_data = "user-data.txt"
   #key_name = aws_key_pair.ssh-key.key_name
   lifecycle {
     create_before_destroy = true
