5f9e5a65caffd11b53cf579dd192cbfd66472030
bobbyryu <bobbyryu@gmail.com>
2022-01-05 00:04:37 -0500

    added bucket name variable

--- variables.tf
+++ variables.tf
@@ -29,12 +29,17 @@ variable "instance_type" {
 }
 
 variable "bucket_name" {
-
+  
 }
 
 variable "bucket_name2" {
-
+  
 }
-variable "bucket_name3" {
 
+variable "bucket_name24" {
+  
+}
+
+variable "bucket_name3" {
+  
 }
